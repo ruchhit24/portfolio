2196cccf2be47176470993065e6d8e7f2230d206
ruchhit24 <ruchit1234.forfirm@gmail.com>
2024-02-11 18:51:44 +0530

    done with installation part

---
+++ src/App.js
@@ -0,0 +1,13 @@
+ 
+function App() {
+  return (
+     <>
+      <div className="text-center text-4xl font-bold">
+    hyy
+    </div>
+    <div className="font-headingFont p-2 m-2 text-4xl font-bold">hrlloe</div>
+     </>
+  );
+}
+
+export default App;
